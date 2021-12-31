a0b8d042bd81425323f60b074182fc0643aea15b
coder2708 <91621292+coder2708@users.noreply.github.com>
2021-12-31 11:32:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-graphics(
-    if (clicked) {
-        alert="ketab"
-    }
-)
+
+        const cookieBox = document.querySelector(".wrapperforcookies"),
+        acceptBtn = cookieBox.querySelector("button");
+        acceptBtn.onclick = ()=>{
+          //setting cookie for 1 month, after one month it'll be expired automatically
+          document.cookie = "CookieBy=ketanpuri; max-age="+60*60*24*30;
+          if(document.cookie){ //if cookie is set
+            cookieBox.classList.add("hide"); //hide cookie box
+          }else{ //if cookie not set then alert an error
+            alert("Cookie can't be set! Please unblock this site from the cookie setting of your browser.");
+          }
+        }
+       //checking our cookie
+        //if cookie is set then hide the cookie box else show it
+        checkCookie != -1 ? cookieBox.classList.add("hide") : cookieBox.classList.remove("hide");
 
